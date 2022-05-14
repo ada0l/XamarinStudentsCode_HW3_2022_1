7d60658a2791ab570ba1815be08be98cb3203388
Maxsman <dekxirus@gmail.com>
2022-05-14 16:46:06 +1000

    Dialog changes, options, backgrounds, stylish text added

--- HW3/HW3/HW3/GameCyclePage.xaml.cs
+++ HW3/HW3/HW3/GameCyclePage.xaml.cs
@@ -14,12 +14,37 @@ namespace HW3
             InitializeComponent();
             _controller = new DialogController();
             currentStepText.Text = _controller.CurrentStep()?.text;
+            background.Source = _controller.GetBackground();
         }
 
         private void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
         {
             _controller.NextStep();
+            var currentStep = _controller.CurrentStep();
+            
+            if (currentStep != null)
+            {
+                currentStepText.Text = currentStep.Value.text;
+            }
+            else
+            {
+                var options = _controller.GetOptions();
+                if(options != null)
+                {
+                    BindableLayout.SetItemsSource(optionSelector, options.Value.selectors);
+                    currentStepText.Text = options.Value.text;
+                }
+            }
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            var dialogTrigger = (sender as Button)?.ClassId;
+            _controller.SetNextDialog(dialogTrigger);
+
             currentStepText.Text = _controller.CurrentStep()?.text;
+            BindableLayout.SetItemsSource(optionSelector, null);
+            background.Source = _controller.GetBackground();
         }
     }
 }
